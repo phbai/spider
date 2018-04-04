b7b212034319d9bb94fffc180c6df1ed65eed71a
phbai <48466846@qq.com>
2018-04-04 22:30:22 +0800

    更新解析逻辑

--- spider/spider.py
+++ spider/spider.py
@@ -36,53 +36,58 @@ def insert_posts_with_page(page):
 
     # 从最后一个item开始解析
     for item in reversed(items):
-        title_info = item.find_all('div')[0]
-        author_info = item.find(target='_parent')
-        
-        # 链接地址、图片地址
-        link = title_info.find('a')['href']
+        try:
+            title_info = item.find_all('div')[0]
+            
+            # 链接地址、图片地址
+            link = title_info.find('a')['href']
+            viewkey = re.findall(r'viewkey=(.+?)&', link)[0]
 
-        sub_page_headers = {'Accept-Language':'zh-CN,zh;q=0.9','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36','X-Forwarded-For':random_ip(),'referer':page_url,'Content-Type': 'multipart/form-data; session_language=cn_CN'}
-        # 子页面抓取描述和时间
-        sub_page_soup = get_soup(link, sub_page_headers)
-        description = sub_page_soup.find_all(class_='more')[0].get_text(strip=True)
-        time = sub_page_soup.find_all('span', class_='title')[1].string
+            sub_page_headers = {'Accept-Language':'zh-CN,zh;q=0.9','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36','X-Forwarded-For':random_ip(),'referer':page_url,'Content-Type': 'multipart/form-data; session_language=cn_CN'}
+            # 子页面抓取描述和时间
+            sub_page_soup = get_soup(link, sub_page_headers)
+            description = sub_page_soup.find_all(class_='more')[0].get_text(strip=True)
+            time = sub_page_soup.find_all('span', class_='title')[1].string
+            author_info = sub_page_soup.find_all(id='videodetails-content')[0].find_all('a')[0]
+            author_url = author_info['href']
+            author = author_info.find('span').get_text().strip()
 
-        # 检测是否已经存入数据库
-        link_result = posts.find_one({"link": link})
-        # 数据库中已经存在 直接跳过
-        if (link_result):
-            continue
+            # 检测是否已经存入数据库
+            link_result = posts.find_one({"viewkey": viewkey})
+            # 数据库中已经存在 直接跳过
+            if (link_result):
+                continue
 
-        img_url = title_info.find('img', width=True)['src']
-        title = title_info.find('img', width=True)['title']
-        author = author_info.string
-        author_url = author_info['href']
-        # 
-        all_texts = re.findall(r'</span>(.+?)[<|\n]', str(item))
-        duration = re.sub(r'\s', '' , all_texts[0])
-        views = re.sub(r'\s', '' , all_texts[2])
-        favorites = re.sub(r'\s', '' , all_texts[3])
-        comments = re.sub(r'\s', '' , all_texts[4])
-        points = re.sub(r'\s', '' , all_texts[5])
-        # print(all_texts)
+            img_url = title_info.find('img', width=True)['src']
+            title = title_info.find('img', width=True)['title']
+            # 
+            all_texts = re.findall(r'</span>(.+?)[<|\n]', str(item))
+            duration = re.sub(r'\s', '' , all_texts[0])
+            views = re.sub(r'\s', '' , all_texts[2])
+            favorites = re.sub(r'\s', '' , all_texts[3])
+            comments = re.sub(r'\s', '' , all_texts[4])
+            points = re.sub(r'\s', '' , all_texts[5])
+            # print(all_texts)
 
-        post = {
-            'title': title, 
-            'link': link, 
-            'author': author, 
-            'author_url': author_url, 
-            'thumbnail': img_url, 
-            'duration': duration, 
-            'time': time, 
-            'views': views, 
-            'favorites': favorites, 
-            'comments': comments, 
-            'points': points,
-            'description': description,
-        }
-        insert_post(post)
-        insert_count = insert_count + 1
+            post = {
+                'title': title, 
+                'link': link, 
+                'viewkey': viewkey,
+                'author': author, 
+                'author_url': author_url, 
+                'thumbnail': img_url, 
+                'duration': duration, 
+                'time': time, 
+                'views': views, 
+                'favorites': favorites, 
+                'comments': comments, 
+                'points': points,
+                'description': description,
+            }
+            insert_post(post)
+            insert_count = insert_count + 1
+        except:
+            pass
         # print('标题：{0} 链接：{1} 作者：{2}'.format(title, link, author))
         # print('时长：{0} 时间：{1} 查看：{2} 收藏：{3} 评论：{4} 积分：{5}'.format(duration, time, views, favorites, comments, points))
     # print(time, views, comments, points)
